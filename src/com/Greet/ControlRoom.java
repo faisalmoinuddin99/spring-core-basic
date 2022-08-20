e0b43d2259482d1c564373ac2e4d714dc0758184
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-20 12:59:49 +0530

    Spring Bean Scope - Singleton

--- src/com/Greet/ControlRoom.java
+++ src/com/Greet/ControlRoom.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ControlRoom {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.Greet");
-        GreetingService greetingService = (GreetingService) context.getBean("greetInEnglish");
-        greetingService.greet("faisal");
+        GreetingService greetingService1 = (GreetingService) context.getBean("greetInEnglish");
+        GreetingService greetingService2 = (GreetingService) context.getBean("greetInEnglish") ;
+//        greetingService1.greet("faisal");
+        System.out.println(greetingService1 == greetingService2); // true
     }
 }
--- src/com/Greet/GreetInEnglish.java
+++ src/com/Greet/GreetInEnglish.java
@@ -3,9 +3,13 @@ package com.Greet;
 import com.HomeWork.Time.TimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class GreetInEnglish implements GreetingService{
 
     @Autowired
